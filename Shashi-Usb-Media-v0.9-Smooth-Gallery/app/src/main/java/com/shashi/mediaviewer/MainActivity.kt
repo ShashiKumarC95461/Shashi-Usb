a2fd02f32f60ef81e52574e2ac375092ef03efed
ShashiKumarC95461 <128719217+ShashiKumarC95461@users.noreply.github.com>
2026-08-18 19:10:57 +0530

    Add setContent import to MainActivity

--- Shashi-Usb-Media-v0.9-Smooth-Gallery/app/src/main/java/com/shashi/mediaviewer/MainActivity.kt
+++ Shashi-Usb-Media-v0.9-Smooth-Gallery/app/src/main/java/com/shashi/mediaviewer/MainActivity.kt
@@ -11,6 +11,7 @@ import android.provider.MediaStore
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 
 import androidx.compose.foundation.background
